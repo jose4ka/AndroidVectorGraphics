6b11756a1e173ca2996bee83f6c67d301f7d0f05
d.garmyshev <d.garmyshev@vladtrial.com>
2021-05-05 11:25:57 +1000

    + Зачатки нового главного меню

---
+++ app/src/main/java/ru/ecostudiovl/vectorgraphics/activity/ActivityMainMenu.java
@@ -0,0 +1,16 @@
+package ru.ecostudiovl.vectorgraphics.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.ecostudiovl.vectorgraphics.R;
+
+public class ActivityMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
