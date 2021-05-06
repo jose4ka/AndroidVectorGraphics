aa9e653f86aad932f364e9a773a5d03d43a63f7d
Dmitriy <rabowvdk125@gmail.com>
2021-05-06 21:37:42 +1000

    + Переделан алгоритм выбора фигуры по линии + Добавлены комментарии

--- app/src/main/java/ru/ecostudiovl/vectorgraphics/view/DrawView.java
+++ app/src/main/java/ru/ecostudiovl/vectorgraphics/view/DrawView.java
@@ -2,18 +2,19 @@ package ru.ecostudiovl.vectorgraphics.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
+import java.util.Map;
+import java.util.TreeMap;
 
 import ru.ecostudiovl.vectorgraphics.fragment.work.FragmentDrawer;
 import ru.ecostudiovl.vectorgraphics.pointsystem.JPoint;
 import ru.ecostudiovl.vectorgraphics.pointsystem.JPointData;
+import ru.ecostudiovl.vectorgraphics.pointsystem.figures.JFigure;
 
 public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
@@ -23,6 +24,11 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
     private int selectedFigure; //Выбранная фигура на данный момент.
     private int touchedPoint; //Индекс точки, на которую мы тыкнули пальцем.
     private boolean isPointTouched; //Переменная нужна при перемещении точки, чтобы не цеплять точки которые очень рядом.
+    private TreeMap<Float, List<JPoint>> pairsMap; /*Мапа, которая используется для поиска пар точек
+    Ключ - расстояние между точками
+    Значение - список с самими точками*/
+
+
 
     /*
     TODO: реализовать нормальное перемещение по холсту, и масштабирование
@@ -34,17 +40,18 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
     private DrawThread drawThread;//Главный поток, отвечающий за постоянную отрисовку фигур и точек (рендеринг).
 
+
+
+    //Интерфейс коллбэка, который нужен для обращения к фраменту
     public interface DrawViewCallback{
         void onSelectFigure(int index);
     }
 
     private DrawViewCallback drawViewCallback;
 
-
-
     public DrawView(Context context, DrawViewCallback drawViewCallback) {
         super(context);
-        this.drawViewCallback = drawViewCallback;
+
         getHolder().addCallback(this);
         initializeVariables();
     }
@@ -54,6 +61,8 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
         selectedFigure = -1; //Изначально -1, т.к. фигур никаких нет
         touchedPoint = -1; //Изначально -1, т.к. точек никаких нет
         isPointTouched = false;
+        this.drawViewCallback = drawViewCallback;
+        this.pairsMap = new TreeMap<>();
     }
 
 
@@ -99,8 +108,12 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
         //Если у нас есть хоть какие-то фигуры, значит уже можем что-то делать с точками
         if (JPointData.getInstance().getFigures().size() > 0){
             switch (mode) {
-                case create:
-
+                case create:/*
+                    Режим создания точек
+                    Срабатывает если у нас выбрана хоть какая-то фигура
+                    Далее, мы получаем шаблон у выбранной фигуры, и смотрим ограничения по точкам у этого шаблона
+                    Если есть ограничения - добавляем точки с учетом ограничений
+                    Если ограничений нет - просто добавляем точки*/
                     if (selectedFigure != -1) {
                         int template = JPointData.getInstance().getFigures().get(selectedFigure).getTemplateIndex();
                         int pointsCount = JPointData.getInstance().getFigures().get(selectedFigure).getPoints().size();
@@ -117,7 +130,9 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
                     }
 
                     return false;
-                case edit:
+                case edit:/*
+                Режим редактирования точек
+                Срабатывает только если мы выбрали какую-то фигуру*/
 
                     if (selectedFigure != -1){
 
@@ -156,6 +171,9 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
                     return true;
 
                 case delete:
+                    /*
+                    Режим удаления точек
+                    Срабатывает только если мы выбрали какую-то фигуру*/
                     if (selectedFigure != -1){
                         touchedPoint = getTouchedPoint(x, y);
                         if (touchedPoint != -1){
@@ -167,15 +185,16 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
                     return false;
 
-                case view:
+                case view:/*
+                Режим простого просмотра фигур
+                Тут мы можем выбрать фигуру с помощью нажатия на точку или линию
+                */
                     touchedPoint = getTouchedPoint(x, y);
-                    Log.i(TAG, "onTouchEvent: "+touchedPoint);
                     if (touchedPoint == -1){
                         findFigureByLine(x, y);
                     }
                     else {
                         selectFigureByPoint(findFigureByPoint(touchedPoint));
-                        
                     }
 
                     return false;
@@ -210,6 +229,39 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
         return result;
     }
 
+    /*
+    Процедура находит фигуру, на которую мы нажали
+    На вход передаётся координаты x и y, где было произведено нажатие
+    1. Находим все пары точек, в зоне которых находятся координаты
+    2. Находим пару точек, расстояние между которыми минимальное
+    3. Если такая пара найдена, находим угол поворота между первой точкой, и координатами
+    4. Далее, находим угол поворота между точками в паре
+    5. Если угол координат, совпадает с углом второй точки (с учётом погрешности),
+     то мы нашли нужное нам пересечение с линией
+    6. Находим фигуру, благодаря процедуре которая находит фигуру по точке (используем первую точку в паре)
+     */
+    private void findFigureByLine(float x, float y){
+        findCollisedPairs(x, y);
+
+        List<JPoint> minimalPair = findMinimalPair();
+
+        if (minimalPair != null){
+            JPoint sourcePoint = minimalPair.get(0);
+            JPoint destPoint = minimalPair.get(1);
+
+            float currPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), x, y);
+
+            float nextPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), destPoint.getX(), destPoint.getY());
+
+            if ((currPDegrees <= nextPDegrees + 20) && (currPDegrees >= nextPDegrees - 20)){
+                selectedFigure = findFigureByPoint(getTouchedPoint(sourcePoint.getX(), sourcePoint.getY()));
+                if (selectedFigure != -1){
+                    drawViewCallback.onSelectFigure(selectedFigure);
+                }
+
+            }
+        }
+    }
 
     /*
     Данная процедура минимизирует индексы точек которые ссылаются
@@ -228,6 +280,9 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+    /*
+    Процедура удаляет фигуру, а так-же все точки которые ей принадлежат
+     */
     public void deletePointsWithFigure(int figureIndex){
         for (int i = 0; i < JPointData.getInstance().getFigures().get(figureIndex).getPoints().size() ; i++) {
             JPointData.getInstance().getPoints().remove(JPointData.getInstance().getFigures().get(figureIndex).getPoints().get(i).intValue());
@@ -235,21 +290,22 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+    //Процедура находит блину отрезка между двумя точками
     private float getLength(float startX, float startY, float endX, float endY){
         return (float) Math.sqrt(Math.pow((endX - startX), 2) + Math.pow((endY - startY), 2));
     }
 
+    //Процедура находит угол поворота одной точки, относительно другой
     private float getDegrees(float aX, float aY, float bX, float bY){
-
-
         double angle = Math.toDegrees(Math.atan2(bX - aX, bY - aY));
-        // Keep angle between 0 and 360
         angle = angle + Math.ceil( -angle / 360 ) * 360;
-
         return (float) angle;
     }
 
 
+    /*
+    Процедура находит самую ближайшую точку к переданным координатам
+     */
     private int findNearPoint(float startX, float startY){
         float minLength = 1000000;
         int index = 0;
@@ -266,97 +322,106 @@ public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
             }
         }
 
-
-
         if (isFinded){
-            Log.i(TAG, "findNearPoint length : "+index + " l = "+minLength);
-//            Log.i(TAG, "findNearPoint degrees : "+getDegrees(
-//                    startX,
-//                    startY,
-//                    JPointData.getInstance().getPoints().get(index).getX(),
-//                    JPointData.getInstance().getPoints().get(index).getY()));
             return index;
         }
         else {
-            Log.i(TAG, "findNearPoint: POINTS NOT FOUND");
             return  -1;
         }
 
     }
 
 
-    private void findFigureByLine(float x, float y){
-        int pointIndex = findNearPoint(x, y);
-        float acceptZone = 20;
-        if (pointIndex != -1){
+    /*
+    Процедура находит все пары точек, в зоне области которых находятся переданные координаты
+     */
+    private void findCollisedPairs(float x, float y){
+        pairsMap.clear();
+        for (int i = 0; i < JPointData.getInstance().getFigures().size(); i++) {
+            JFigure figure = JPointData.getInstance().getFigures().get(i);
+            for (int j = 0; j < figure.getPoints().size() - 1; j++) {
+                JPoint startPoint = JPointData.getInstance().getPoints().get(figure.getPoints().get(j));
+                JPoint endPoint = JPointData.getInstance().getPoints().get(figure.getPoints().get(j+1));
 
-            int figureIndex = findFigureByPoint(pointIndex);
+                if (isPointInZone(x, y, startPoint, endPoint)){
+                    List<JPoint> lList = new ArrayList<>();
+                    lList.add(startPoint);
+                    lList.add(endPoint);
 
-            if (figureIndex != -1){
-                int localPointIndex = JPointData.getInstance().getFigures().get(figureIndex).getLocalIndexOfPoint(pointIndex);
-
-                JPoint sourcePoint = JPointData.getInstance().getPoints().get(
-                        JPointData.getInstance().getFigures().get(figureIndex).getPoints().get(localPointIndex)
-                );
-
-                float currPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), x, y);
-
-                JPoint nextPoint, prevPoint;
-                if (localPointIndex == 0){
-
-                    nextPoint = JPointData.getInstance().getPoints().get(
-                            JPointData.getInstance().getFigures().get(figureIndex).getPoints().get(localPointIndex + 1)
-                    );
-                    float nextPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), nextPoint.getX(), nextPoint.getY());
-
-
-
-                    if ((currPDegrees <= nextPDegrees + acceptZone) && (currPDegrees >= nextPDegrees - acceptZone)){
-                        selectedFigure = figureIndex;
-                        drawViewCallback.onSelectFigure(selectedFigure);
-                    }
-
+                    float length = getLength(startPoint.getX(), startPoint.getY(), endPoint.getX(), endPoint.getY());
+                    Float oLength = new Float(length);
+                    pairsMap.put(oLength, lList);
                 }
-                else if(localPointIndex == JPointData.getInstance().getFigures().get(figureIndex).getPoints().size() - 1){
 
-                    prevPoint = JPointData.getInstance().getPoints().get(
-                            JPointData.getInstance().getFigures().get(figureIndex).getPoints().get(localPointIndex - 1)
-                    );
-                    float prevPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), prevPoint.getX(), prevPoint.getY());
-
-                    if ((currPDegrees <= prevPDegrees + acceptZone) && (currPDegrees >= prevPDegrees - acceptZone)){
-                        selectedFigure = figureIndex;
-                        drawViewCallback.onSelectFigure(selectedFigure);
-                    }
-                }
-                else {
-                    nextPoint = JPointData.getInstance().getPoints().get(
-                            JPointData.getInstance().getFigures().get(figureIndex).getPoints().get(localPointIndex + 1)
-                    );
-                    float nextPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), nextPoint.getX(), nextPoint.getY());
-
-                    prevPoint = JPointData.getInstance().getPoints().get(
-                            JPointData.getInstance().getFigures().get(figureIndex).getPoints().get(localPointIndex - 1)
-                    );
-                    float prevPDegrees = getDegrees(sourcePoint.getX(), sourcePoint.getY(), prevPoint.getX(), prevPoint.getY());
-
-                    if ((currPDegrees <= nextPDegrees + acceptZone) && (currPDegrees >= nextPDegrees - acceptZone)){
-                        selectedFigure = figureIndex;
-                        drawViewCallback.onSelectFigure(selectedFigure);
-                    }
-                    else if ((currPDegrees <= prevPDegrees + acceptZone) && (currPDegrees >= prevPDegrees - acceptZone)){
-                        selectedFigure = figureIndex;
-                        drawViewCallback.onSelectFigure(selectedFigure);
-                    }
-                }
             }
-
-
         }
     }
 
-    private int findFigureByPoint(int point){
+    /*
+    Процедура находит пару точек, расстояние между которыми минимальное
+     */
+    private List<JPoint> findMinimalPair(){
 
+        Map.Entry<Float, List<JPoint>> minEntry = null;
+        for (Map.Entry<Float, List<JPoint>> entry : pairsMap.entrySet()) {
+            if (minEntry == null || entry.getKey() < minEntry.getKey()) {
+                minEntry = entry;
+            }
+        }
+        if (minEntry == null){
+            return null;
+        }
+        List<JPoint> pair = minEntry.getValue();
+
+        return pair;
+    }
+
+    /*
+    Процедура проверяет нахождение точки в прямоугольной области
+    Учитывается то, в каком направлении идёт фигура
+    Вверх, вниз, влево, вправо
+     */
+    private boolean isPointInZone(float x, float y, JPoint startPoint, JPoint endPoint){
+
+        boolean result = false;
+        if (startPoint.getX() <= endPoint.getX()){
+
+            if (startPoint.getY() <= endPoint.getY()){
+                if (startPoint.getX() <= x && x <= endPoint.getX() &&
+                        startPoint.getY() <= y && y <= endPoint.getY()){
+                    result = true;
+                }
+            }
+            else {
+                if(startPoint.getX() <= x && x <= endPoint.getX() &&
+                        startPoint.getY() >= y && y >= endPoint.getY()){
+                    result = true;
+                }
+            }
+
+        }
+        else {
+            if (startPoint.getY() <= endPoint.getY()){
+                if (startPoint.getX() >= x && x >= endPoint.getX() &&
+                        startPoint.getY() <= y && y <= endPoint.getY()){
+                    result = true;
+                }
+            }
+            else {
+                if(startPoint.getX() >= x && x >= endPoint.getX() &&
+                        startPoint.getY() >= y && y >= endPoint.getY()){
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /*
+    Процедура находит фигуру, которой принадлежит индекс переданной точки
+     */
+    private int findFigureByPoint(int point){
         for (int i = 0; i < JPointData.getInstance().getFigures().size(); i++) {
             if (JPointData.getInstance().getFigures().get(i).isContainsPoint(point)){
                 return i;
